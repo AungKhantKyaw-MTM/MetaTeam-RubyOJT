6d579f552dafc15adbf93fd2d587b026c43882bf
AungKhantKyaw-MTM <mtm.aungkhantkyaw@gmail.com>
2024-08-06 15:12:25 +0630

    tuto 2

---
+++ tuto2.rb
@@ -0,0 +1,21 @@
+require 'date'
+
+def calculate_age(date_of_birth)
+    today = Date.today
+    age = today.year - date_of_birth.year
+
+    if today.month < date_of_birth.month || (today.month == date_of_birth.month && today.day < date_of_birth.day)
+        age -= 1
+    end
+    puts ("Your age is " + age.to_s)
+end
+
+puts "Enter Birth Date: "
+birth_date = gets.chomp()
+puts "Enter Birth Month: "
+birth_month = gets.chomp()
+puts "Enter Birth Year: "
+birth_year = gets.chomp()
+
+dob = Date.new(birth_year.to_i, birth_month.to_i, birth_date.to_i) 
+puts calculate_age(dob)
